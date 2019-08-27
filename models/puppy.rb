803d47eb7a4d5e5f41d3c40b84770ec31bfa3751
David Forer <david.j.forer@gmail.com>
2019-08-27 02:00:37 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -3,7 +3,7 @@ class Puppy
   
   @@all = []
   
-  def initialize(:name, :breed, :months_old)
+  def initialize(name, breed, months_old)
     @name = name
     @breed = breed
     @months_old = months old
